76acfed77c6a9b1f2c9ce097e6ae0cdbf868447b
stupxd <uknownvector@gmail.com>
2026-05-24 03:10:44 +0300

    update modicon resolution

--- HouseOfHearts.lua
+++ HouseOfHearts.lua
@@ -23,8 +23,8 @@ SMODS.Atlas {
 SMODS.Atlas {
     key = 'modicon',
     path = 'modicon.png',
-    px = 154,
-    py = 154,
+    px = 34,
+    py = 34,
     atlas_table = 'ASSET_ATLAS'
 }
 SMODS.Atlas {
